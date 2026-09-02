71724517da64e15d492d2539ec5b77149e6652ce
rempale <saleksandr2007@gmail.com>
2026-09-02 11:43:55 +0300

    Task #1

---
+++ helloworld/src/main/java/com/devops/helloworld/HelloController.java
@@ -0,0 +1,14 @@
+package com.devops.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping
+    public String getHelloDevOps() {
+        return "HelloDevOps";
+    }
+
+}
